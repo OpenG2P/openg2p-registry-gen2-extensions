8106827724033706318ef369bdc5a307de1e9943
PSNAppZ <thepsnarayanan@gmail.com>
2026-03-09 17:33:09 +0530

    Add db scripts for farmer register reference

---
+++ openg2p-registry-farmer-extension/src/openg2p_registry_farmer_extension/db_scripts/configuration_data/g2p_attribute_values.sql
@@ -0,0 +1,10 @@
+INSERT INTO "public"."g2p_attribute_values" ("value_id", "attribute_id", "value_code", "value_display", "parent_value_id", "sort_order") VALUES
+('DRIP_IRRIGATION', 'WATER_SOURCE', 'DRIP_IRRIGATION', 'Drip Irrigation', NULL, 1),
+('GROUND_WATER', 'WATER_SOURCE', 'GROUND_WATER', 'Ground Water', NULL, 3),
+('HOUSEHOLD_COMPOST', 'FERTILIZER_TYPE', 'HOUSEHOLD_COMPOST', 'Household Compost', NULL, 1),
+('MUNICIPAL_WATER', 'WATER_SOURCE', 'MUNICIPAL_WATER', 'Municipal Water', NULL, 5),
+('NPK', 'FERTILIZER_TYPE', 'NPK', 'NPK', NULL, 2),
+('POULTRY_WASTE', 'FERTILIZER_TYPE', 'POULTRY_WASTE', 'Poultry Waste', NULL, 4),
+('RAIN_WATER', 'WATER_SOURCE', 'RAIN_WATER', 'Rain Water', NULL, 2),
+('RIVER', 'WATER_SOURCE', 'RIVER', 'River', NULL, 4),
+('UREA', 'FERTILIZER_TYPE', 'UREA', 'Urea', NULL, 3);
---
+++ openg2p-registry-farmer-extension/src/openg2p_registry_farmer_extension/db_scripts/configuration_data/g2p_attributes.sql
@@ -0,0 +1,3 @@
+INSERT INTO "public"."g2p_attributes" ("attribute_id", "attribute_code", "attribute_display", "is_hierarchical") VALUES
+('FERTILIZER_TYPE', 'FERTILIZER_TYPE', 'Fertilizer Type', 'f'),
+('WATER_SOURCE', 'WATER_SOURCE', 'Water Source', 'f');
