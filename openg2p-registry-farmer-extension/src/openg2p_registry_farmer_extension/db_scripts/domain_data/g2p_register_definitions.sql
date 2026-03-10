ed87d1167b23e703e906e42fd54a09f5b8ae1a14
PSNAppZ <thepsnarayanan@gmail.com>
2026-03-10 10:04:19 +0530

    Remove obsolete SQL seed data files for g2p register definitions, schemas, sections, document labels, and UI tabs.

--- openg2p-registry-farmer-extension/src/openg2p_registry_farmer_extension/db_scripts/domain_data/g2p_register_definitions.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO "public"."g2p_register_definitions" ("register_id", "register_mnemonic", "register_subject", "register_description", "master_register_id", "register_rank", "register_purpose", "program_id", "program_mnemonic", "register_icon", "has_image", "dedup_is_enabled", "dedup_threshold_score") VALUES
-('1349e161-377b-4b4b-88cf-49b54c9723fa', 'HouseholdMember', 'Household Members', 'Household Member Register', '49525ce4-d814-463c-9bbd-4582caf1b3ad', NULL, 'TABLE', NULL, NULL, NULL, 'f', 'f', 0),
-('3f216e71-2125-4b9a-9cb3-011ffc3c50cd', 'Machinery', 'Machinery', 'Machinery Register1', '6428deca-575d-41f0-b022-a0d79f1495f4', 0, 'TABLE', NULL, NULL, '', 'f', 'f', 0),
-('49525ce4-d814-463c-9bbd-4582caf1b3ad', 'Household', 'Households', 'Household Register', NULL, 10, 'REGISTER', NULL, NULL, 'data:image/png;
-
-INSERT INTO "public"."g2p_register_definitions" ("register_id", "register_mnemonic", "register_subject", "register_description", "master_register_id", "register_rank", "register_purpose", "program_id", "program_mnemonic", "register_icon", "has_image", "dedup_is_enabled", "dedup_threshold_score") VALUES
-('ba5ec931-1a8c-4cc7-bdac-a1534954072f', 'Land', 'Lands', 'Land Register', '6428deca-575d-41f0-b022-a0d79f1495f4', 100, 'TABLE', NULL, NULL, 'data:image/png;
