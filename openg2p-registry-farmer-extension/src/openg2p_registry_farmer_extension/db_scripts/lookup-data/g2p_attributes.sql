f63a8b4f77fbffe896d724f66031720fa9c0a395
tahzeer <tahzeer.ash@gmail.com>
2026-04-06 17:32:57 +0530

    feat(data): update seed data

--- openg2p-registry-farmer-extension/src/openg2p_registry_farmer_extension/db_scripts/lookup-data/g2p_attributes.sql
+++ openg2p-registry-farmer-extension/src/openg2p_registry_farmer_extension/db_scripts/lookup-data/g2p_attributes.sql
@@ -1,8 +1,9 @@
-INSERT INTO "public"."g2p_attributes" ("attribute_id", "attribute_code", "attribute_display", "is_hierarchical") VALUES
-('SOURCE_OF_INCOME', 'SOURCE_OF_INCOME', 'Source of Income', 'f'),
-('CROP_COMMODITY', 'CROP_COMMODITY', 'Crop Commodity', 'f'),
-('CROP_SEASON', 'CROP_SEASON', 'Crop Season', 'f'),
-('LIVESTOCK_TYPE', 'LIVESTOCK_TYPE', 'Livestock Type', 'f'),
-('LIVESTOCK_BREED', 'LIVESTOCK_BREED', 'Livestock Breed', 'f'),
-('WATER_SOURCE', 'WATER_SOURCE', 'Water Source', 'f'),
-('MEANS_OF_ACQUISITION', 'MEANS_OF_ACQUISITION', 'Means of Acquisition', 'f');
+INSERT INTO "public"."g2p_attributes" ("attribute_id","attribute_code","attribute_display","is_hierarchical") VALUES 
+('SOURCE_OF_INCOME','SOURCE_OF_INCOME','Source of Income','FALSE'),
+('CROP_COMMODITY','CROP_COMMODITY','Crop Commodity','FALSE'),
+('CROP_SEASON','CROP_SEASON','Crop Season','FALSE'),
+('LIVESTOCK_TYPE','LIVESTOCK_TYPE','Livestock Type','FALSE'),
+('LIVESTOCK_BREED','LIVESTOCK_BREED','Livestock Breed','FALSE'),
+('WATER_SOURCE','WATER_SOURCE','Water Source','FALSE'),
+('MEANS_OF_ACQUISITION','MEANS_OF_ACQUISITION','Means of Acquisition','FALSE'),
+('SOIL_FERTILITY','SOIL_FERTILITY','Soil Fertility','FALSE');
